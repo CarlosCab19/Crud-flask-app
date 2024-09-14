4de4e656b0026bd49302dbe6e5f33a66c24bfd89
Carlos <cabtuncarlos@gmail.com>
2024-09-14 12:57:22 -0600

    avances del curso

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\carlos.cab\Documents\GitHub\my-app-flask\env
+set VIRTUAL_ENV=C:\Users\carlos.cab\Documents\GitHub\Crud-flask-app\env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- todor/__init__.py
+++ todor/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
 
@@ -7,8 +10,12 @@ def create_app():
     #Configuración del proyecto
     app.config.from_mapping(
         DEBUG = True,
-        SECRETE_KEY = 'dev'
+        SECRETE_KEY = 'dev',
+        SQLALCHEMY_DATABASE_URI = "sqlite:///todolisto.db"
     )
+
+    #Inicializamos nuestra conexion a nuestra bd
+    db.init_app(app)
 
     #Registrar Bluprint
     from . import todo
@@ -20,5 +27,8 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    with app.app_context():
+        db.create_all()
     
     return app
